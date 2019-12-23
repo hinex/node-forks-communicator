980467d811d7e92955c5b9a277cb09ce9ceaec68
Roman Hinex <roman@hinex.io>
2019-12-24 01:57:18 +0600

    Add Worker support

--- example/fork.js
+++ example/fork.js
@@ -3,8 +3,8 @@ const { fork } = require("child_process");
 const { join } = require("path");
 
 // Wrap forks in the communicator
-setup(fork(join(__dirname, "/child/fork.js")));
-setup(fork(join(__dirname, "/child/another_fork.js")));
+setup(fork(join(__dirname, "/child/process.js")));
+setup(fork(join(__dirname, "/child/another_process.js")));
 
 // Subscribe to channels from master process
 subscribe("say", ({ message }) => console.log(`[master] recived: ${message}`));
--- example/worker.js
+++ example/worker.js
@@ -3,8 +3,8 @@ const { Worker } = require("worker_threads");
 const { join } = require("path");
 
 // Wrap forks in the communicator
-setup(new Worker(join(__dirname, "/child/fork.js")));
-setup(new Worker(join(__dirname, "/child/another_fork.js")));
+setup(new Worker(join(__dirname, "/child/process.js")));
+setup(new Worker(join(__dirname, "/child/another_process.js")));
 
 // Subscribe to channels from master process
 subscribe("say", ({ message }) => console.log(`[master] recived: ${message}`));
